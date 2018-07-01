ba610cc1d27e05c3655d1d617301f5d70bd03c9f
heiang <598481337@qq.com>
2018-07-01 12:38:47 +0800

    提交

---
+++ autoBuild.js
@@ -0,0 +1,31 @@
+var http = require('http')
+var spawn = require('child_process').spawn
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/pushCode', secret: '' }) // 在代码仓库的 Webhooks 选项处配置
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 404;
+        res.end('no such location')
+    })
+}).listen(7777)
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message)
+})
+
+// 监听 push 事件
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref)
+    rumCommand('sh', ['./autoBuild.sh'], function( txt ) { // 执行 autoBuild.sh 脚本文件
+        console.log(txt)
+    })
+})
+
+function rumCommand( cmd, args, callback ) {
+    var child = spawn( cmd, args )
+    var response = ''
+    child.stdout.on('data', function( buffer ){ response += buffer.toString(); })
+    child.stdout.on('end', function(){ callback( response ) })
+}
